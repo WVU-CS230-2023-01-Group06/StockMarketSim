753224108c63a73c2ef2b2c27b6df18c345651ed
kcoding03 <87551961+kcoding03@users.noreply.github.com>
2023-03-03 23:52:23 -0500

    Create and Route Basic Pages
    Creation of Log In, Sign In, and Homepage along with routing to all pages

--- StockMarketSim/src/app/app-routing.module.ts
+++ StockMarketSim/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { PriceFormComponent } from './price-form/price-form.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LogInComponent
+   },
+   {
+    path: 'CreateAccount',
+    component: CreateAccountComponent
+   },
+   {
+    path: 'Homepage',
+    component: UserPageComponent
+   },
+   {
+    path: 'StockSearch',
+    component:PriceFormComponent
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- StockMarketSim/src/app/app.module.ts
+++ StockMarketSim/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PriceFormComponent } from './price-form/price-form.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
-  declarations: [AppComponent, PriceFormComponent],
+  declarations: [AppComponent, PriceFormComponent, LogInComponent, CreateAccountComponent, UserPageComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
