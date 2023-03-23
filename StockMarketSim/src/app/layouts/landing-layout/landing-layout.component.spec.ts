4c4805ecf128d83d9d70e3a2352644fba1600e9b
kcoding03 <87551961+kcoding03@users.noreply.github.com>
2023-03-22 22:52:04 -0400

    Addition of News API servie for HomePage

--- StockMarketSim/src/app/layouts/landing-layout/landing-layout.component.spec.ts
+++ StockMarketSim/src/app/layouts/landing-layout/landing-layout.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LandingLayoutComponent } from './landing-layout.component';
 
 describe('LandingLayoutComponent', () => {
